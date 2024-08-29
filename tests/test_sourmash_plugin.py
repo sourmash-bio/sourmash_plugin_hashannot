42c4d0d35de13339fe1272fd5ae510a6c39b2b35
C. Titus Brown <titus@idyll.org>
2024-08-29 07:26:49 -0700

    some tests, that also pass

--- tests/test_sourmash_plugin.py
+++ tests/test_sourmash_plugin.py
@@ -16,3 +16,40 @@ def test_run_sourmash(runtmp):
     print(runtmp.last_result.out)
     print(runtmp.last_result.err)
     assert runtmp.last_result.status != 0                    # no args provided, ok ;)
+
+
+def test_simple(runtmp):
+    shew47 = utils.get_test_data('shew-47.500k.fa.gz')
+    shew63 = utils.get_test_data('shew-63.500k.fa.gz')
+
+    # sketch 47
+    sig47 = runtmp.output('47.sig.zip')
+    runtmp.sourmash('sketch', 'dna', shew47, '-o', sig47)
+
+    # extract from 63
+    contigs = runtmp.output('out.fa')
+    runtmp.sourmash('scripts', 'extract_surrounding', shew63, sig47,
+                    '-o', contigs)
+
+    out = runtmp.last_result.out
+    assert "original 500000, extracted 293015" in out
+
+    assert os.path.exists(contigs)
+
+
+def test_simple_identical(runtmp):
+    shew47 = utils.get_test_data('shew-47.500k.fa.gz')
+
+    # sketch 47
+    sig47 = runtmp.output('47.sig.zip')
+    runtmp.sourmash('sketch', 'dna', shew47, '-o', sig47)
+
+    # extract from 47
+    contigs = runtmp.output('out.fa')
+    runtmp.sourmash('scripts', 'extract_surrounding', shew47, sig47,
+                    '-o', contigs)
+
+    out = runtmp.last_result.out
+    assert "original 500000, extracted 500000" in out
+
+    assert os.path.exists(contigs)
